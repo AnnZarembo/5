98b568901a2ffa40d14b04b3b30ea97831546de1
AnnZarembo <146215603+AnnZarembo@users.noreply.github.com>
2023-12-23 17:47:31 +0500

    исправила ошибки

--- ШифрЦезаря.py
+++ ШифрЦезаря.py
@@ -1,58 +1,39 @@
-def shifr(text,shift):
-    shifr_text=''
+def shifr(text, shift):
+    shifr_text = ''
     for i in text:
-        if i.isupper():
-            i_index=ord(i) - ord('А')
-            i_shifted=(i_index + shift) % 26 + ord('А')
-            i_new=chr(i_shifted)
-            shifr_text+=i_new
-        elif i.islower():
-            i_index=ord(i)-ord('а')
-            i_shifted=(i_index + shift)%26+ord('а')
-            i_new=chr(i_shifted)
-            shifr_text+=i_new
-        elif i.isdigit():
-            i_new=(int(i)+shift)%10
-            shifr_text+=str(i_new)
+        if i.isalpha():
+            start = ord('A') if i.isupper() else ord('a')
+            shifr_char = chr((ord(i) - start + shift) % 26 + start)
+            shifr_text += shifr_char
         else:
             shifr_text+=i
     return shifr_text
 
-def shifr_rashifr(text,shift):
-    rashifr_text=''
+def rashifr(text, shift):
+    rashifr_text = ''
     for i in text:
-        if i.isupper():
-            i_index=ord(i) - ord('А')
-            i_levo_pos=(i_index - shift) % 26 + ord('А')
-            i_levo=chr(i_levo_pos)
-            rashifr_text+=i_levo
-        elif i.islower():
-            i_index=ord(i) - ord('а')
-            i_levo_pos=(i_index - shift) % 26 + ord('а')
-            i_levo=chr(i_levo_pos)
-            rashifr_text+=i_levo
-        elif i.isdigit():
-            i_levo =(int(i)-shift) % 10
-            rashifr_text+=str(i_levo)
+        if i.isalpha():
+            start = ord('A') if i.isupper() else ord('a')
+            rashifr = chr((ord(i) - start - shift) % 26 + start)
+            rashifr_text += rashifr
         else:
-            rashifr_text+=i
+            rashifr_text += i
     return rashifr_text
 
-iscl=' ,.*()_+{["|\/}]№@#$^&;%:%*-=~1234567890!?<>'
+alphabet_of_exceptions = '.!?&*(|\/}]№;%:@#$%^* <>,-=~123456)_+{["7890'
 while True:
-        a_1 = True
-        a_2 = True
-
-        text = input('Введите текст:')
-        for letter in text:
-            if letter=='Ё'or letter=='ё':
-                a_1 = False
-            if letter not in iscl:
-                if ord(letter) < 1040 or ord(letter) > 1103:
-                    a_2 = False
-        if a_1 == False:
+        a1 = True
+        a2 = True
+        text = input('Введите текст: ')
+        for i in text:
+            if i == 'ё' or i == 'Ё':
+                a1 = False
+            if i not in alphabet_of_exceptions:
+                if ord(i) < 1040 or ord(i) > 1103:
+                    a2 = False
+        if a1==False:
             print('Введите текст без буквы ё')
-        elif a_2 == False:
+        elif a2==False:
             print('Введите текст на русском языке')
         else:
             break
@@ -63,7 +44,7 @@ while True:
     except ValueError:
         print('Шаг сдвига должен быть целым числом')
 
-shifr_text=shifr(text,shift)
-print('Зашифрованный текст:',shifr_text)
-shifr_text_text=shifr_rashifr(text,shift)
-print('Расшифрованный текст:',shifr_text)
+shifr_text = shifr(text, shift)
+print('Зашифрованный текст:', shifr_text)
+rashifr_text = rashifr(shifr_text, shift)
+print('Расшифрованный текст:', text)
