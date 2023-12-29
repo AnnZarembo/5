bf3445794c6fb5b1ef77e78da45be5c4ffd834ee
AnnZarembo <146215603+AnnZarembo@users.noreply.github.com>
2023-12-29 13:47:17 +0500

    исправила код , чтоб работал и для русского и английского

--- ШифрЦезаря.py
+++ ШифрЦезаря.py
@@ -1,38 +1,81 @@
-def shifr(text, shift):
-    shifr_text = ""
+def russia(text, shift):
+    russia_text = ''
     for i in text:
         if i.isalpha():
-            start = ord('A') if i.isupper() else ord('a')
-            encrypted_char = chr((ord(i) - start + shift) % 26 + start)
-            shifr_text += encrypted_char
+            if i.isupper():
+                encrypted_char = chr((ord(i) - ord('А') + shift) % 32 + ord('А'))
+            else:
+                encrypted_char = chr((ord(i) - ord('а') + shift) % 32 + ord('а'))
+            russia_text += encrypted_char
         else:
-            shifr_text += i
-    return shifr_text
+            russia_text += i
+    return russia_text
 
-def rashifr(encrypted_text, shift):
-    rashifr_text = ""
-    for i in encrypted_text:
+def rushifr_russia(text, shift):
+    rushifr_russia_text = ''
+    for i in text:
         if i.isalpha():
-            start = ord('A') if i.isupper() else ord('a')
-            decrypted_char = chr((ord(i) - start - shift) % 26 + start)
-            rashifr_text += decrypted_char
+            if i.isupper():
+                decrypted_char = chr((ord(i) - ord('А') - shift) % 32 + ord('А'))
+            else:
+                decrypted_char = chr((ord(i) - ord('а') - shift) % 32 + ord('а'))
+            rushifr_russia_text += decrypted_char
         else:
-            rashifr_text += i
-    return rashifr_text
+            rushifr_russia_text += i
+    return rushifr_russia_text
 
-is_ok_2=False
-while is_ok_2 == False:
-     try:
-          shift = int(input('Введите шаг сдвига (целое число):'))
-          if shift > 0:
-               is_ok_2 = True
-          else:
-               print('шаг должен быть больше 0')
-     except ValueError:
-          print('Шаг сдвига должен быть целым числом')
-         
-text = input("Введите текст: ")
-shifr_text = shifr(text, shift)
-print("Зашифрованный текст: ", shifr_text)
-rashifr_text = rashifr(shifr_text, shift)
-print("Расшифрованный текст: ", text)
+
+def angl(text, shift):
+    angl_text = ""
+    for char in text:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            encrypted_char = chr((ord(char) - start + shift) % 26 + start)
+            angl_text += encrypted_char
+        else:
+            angl_text += char
+    return angl_text
+
+def rashifr_angl(encrypted_text, shift):
+    rashifr_angl_text = ""
+    for char in encrypted_text:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            decrypted_char = chr((ord(char) - start - shift) % 26 + start)
+            rashifr_angl_text += decrypted_char
+        else:
+            rashifr_angl_text += char
+    return rashifr_angl_text
+
+
+
+
+while True:
+    vibor = input("Введите на каком языке делать расшифровку(русский или английский): ")
+    if vibor in ['русский', 'английский']:
+        while True:
+            try:
+                shift = int(input('Введите шаг сдвига (целое число): '))
+                if vibor == 'русский':
+                    text = input('Введите текст: ')
+                    russia_text = russia(text, shift)
+                    print('Зашифрованный текст:', russia_text)
+                    rushifr_russia_text = rushifr_russia(russia_text, shift)
+                    print('Расшифрованный текст:', rushifr_russia_text)
+                    break
+                if vibor == 'английский':
+                    text = input('Введите текст: ')
+                    angl_text = angl(text, shift)
+                    print("Зашифрованный текст: ", angl_text)
+                    rashifr_angl_text = rashifr_angl(angl_text, shift)
+                    print("Расшифрованный текст: ", rashifr_angl_text)
+                    break
+            except ValueError:
+                print('Шаг сдвига должен быть целым числом')
+    else:
+        print("Неправильный ввод,выберите русский или анлийский(пишите с маленькой буквы русскими буквами)")
+
+
+
+
+
